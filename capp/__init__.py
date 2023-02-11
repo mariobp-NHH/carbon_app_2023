60b7a7b2008fc3591016619a96352cfe97818dbc
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-11 11:37:15 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -4,31 +4,29 @@ from botocore.exceptions import ClientError
 
 application = Flask(__name__)
 
-def get_secret():
+secret_name = "carbon-app-2023-secret-key"
+region_name = "eu-north-1"
 
-    secret_name = "carbon-app-2023-secret-key"
-    region_name = "eu-north-1"
+# Create a Secrets Manager client
+session = boto3.session.Session()
+client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name
+)
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
+try:
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
     )
+except ClientError as e:
+    # For a list of exceptions thrown, see
+    # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    raise e
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
+# Decrypts secret using the associated KMS key.
+secret = get_secret_value_response['SecretString']
 
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-
-    # Your code goes here.
+# Your code goes here.
 application.config['SECRET_KEY'] = secret    
 
 # application.config['SECRET_KEY'] = '1dcdsdsd5b2ffa3afggfdgfdgfdgf090dfc34f343rffe845fd'
