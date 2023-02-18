3db0e34332938632572b76bc492e9863333c9004
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-18 12:21:10 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 
 application = Flask(__name__)
 
 application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+
+# Define the databasem in GitHub
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+db = SQLAlchemy(application)
+
+bcrypt = Bcrypt(application)
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
@@ -14,4 +24,3 @@ application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_app)
 application.register_blueprint(users)
-
