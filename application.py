877ce04a8ae21db0483f5febcb6ec0bbb8d09415
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-18 21:46:08 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,23 @@
-from capp import application
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+application = Flask(__name__) 
+
+
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+db = SQLAlchemy(application)
+
+class User(db.Model):
+    __tablename__ = "user_table"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    
+@application.route('/')
+@application.route('/home')
+def home():
+  return "<h1>Welcome Home</h1>"
 
 if __name__=='__main__':
-  application.run(debug=True)  
+  application.run(debug=True) 
