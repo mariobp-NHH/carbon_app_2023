a4d1371737251f71b3fa9ffa3fb4441e571aba26
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-18 12:21:56 +0100

    Update routes.py

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint
+from capp.models import Transport
 
 carbon_app=Blueprint('carbon_app',__name__)
 
@@ -13,4 +14,3 @@ def new_entry():
 @carbon_app.route('/carbon_app/your_data')
 def your_data():
     return render_template('carbon_app/your_data.html', title='your_data')
-    
