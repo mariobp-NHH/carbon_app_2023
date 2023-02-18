e19cb249a8cb54c6124e128d2abd53df41c847d5
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-18 15:42:08 +0100

    Add files via upload

---
+++ capp/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+application = Flask(__name__)
+
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'gender_platform': DBVAR}
+
+# DBVAR = 'sqlite:///user.db'
+# application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
+# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
+# application.config['SQLALCHEMY_BINDS'] ={'gender_platform': 'sqlite:///gender_platform.db'}
+
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager= LoginManager(application)
+login_manager.login_view = 'forward_users.forward_users_login'
+login_manager.login_message_category = 'info'
+
+from capp.home.routes import home
+from capp.methodology.routes import methodology
+from capp.carbon_app.routes import carbon_app
+from capp.users.routes import users
+
+application.register_blueprint(home)
+application.register_blueprint(methodology)
+application.register_blueprint(carbon_app)
+application.register_blueprint(users)
+
