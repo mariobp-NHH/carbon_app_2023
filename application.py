12e5c5d9a812f5256587a5625f0782ed96c8f8d1
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-19 19:33:41 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,14 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__) 
 
-# DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-# db = SQLAlchemy(application)
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+db = SQLAlchemy(application)
 
-# class User(db.Model):
-#   __tablename__ = "user_table"
-#   id = db.Column(db.Integer, primary_key=True)
-#   username = db.Column(db.String(30), unique=True, nullable=False)
+class User(db.Model):
+  __tablename__ = "user_table"
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(30), unique=True, nullable=False)
     
 @application.route('/')
 @application.route('/home')
