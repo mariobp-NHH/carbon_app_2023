29aaabad43acc157d8b06bac92cde84f1e397e5e
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-19 17:37:25 +0100

    Update routes.py

--- capp/home/routes.py
+++ capp/home/routes.py
@@ -27,10 +27,10 @@ def home_home():
 #   db.session.add(transport4) 
 #   db.session.commit()
   
-  user1 = User.query.first().username
-  transport1 = Transport.query.first().kms
-  user3 = User.query.filter_by(username='Bjørk').first().username
-  user4 = User.query.filter_by(username='Bjørk').first()
-  transports = Transport.query.filter_by(user_id=user4.id)
-  users = User.query.all()
-  return render_template('home.html', user1=user1, transport1=transport1, user3=user3, transports=transports, users=users)
+#   user1 = User.query.first().username
+#   transport1 = Transport.query.first().kms
+#   user3 = User.query.filter_by(username='Bjørk').first().username
+#   user4 = User.query.filter_by(username='Bjørk').first()
+#   transports = Transport.query.filter_by(user_id=user4.id)
+#   users = User.query.all()
+  return render_template('home.html')
