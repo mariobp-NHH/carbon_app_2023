038c4f1933416b4fef1140faee9aa8f3f986b5c4
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-03-07 09:57:35 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 application = Flask(__name__)
 
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 
+db = SQLAlchemy(application)
 from capp.home.routes import home
 from capp.methodology.routes import methodology
 from capp.carbon_app.routes import carbon_app
